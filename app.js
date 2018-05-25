ccb3efd1778197bb98ad064e2391c9b6ada91482
l1g4v <user_l1g4v@users.noreply.github.com>
2018-05-25 18:12:39 -0500

    u

--- app.js
+++ app.js
@@ -222,7 +222,8 @@ var server = http.createServer(function (req, res) {
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
         </html>
         `;
-        res.end(resulth);
+                console.log(tbody);
+                res.end(resulth);
 
             }
         });
