35ab1c3da9192899956860538772104bce26a127
l1g4v <user_l1g4v@users.noreply.github.com>
2018-05-25 17:43:44 -0500

    u

--- app.js
+++ app.js
@@ -213,7 +213,7 @@ var server = http.createServer(function (req, res) {
 
     conn = null;
 
-    resulth += `<tbody></tbody></table></div></body>
+    resulth += `${tbod}</table></div></body>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
@@ -230,7 +230,7 @@ var server = http.createServer(function (req, res) {
         `;
     //console.log(resadd);
     //
-    resulth = resulth.replace(/<\/?(tbody)>/g, `${tbod}</tbody>`);;
+    //resulth = resulth.replace(/<\/?(tbody)>/g, `${tbod}</tbody>`);
     //
     console.log(resulth);
     res.end(resulth);
