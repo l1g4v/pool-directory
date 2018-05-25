67ee4c8ef0ed5bc168bd421a9667cae6884a9653
l1g4v <user_l1g4v@users.noreply.github.com>
2018-05-25 18:11:06 -0500

    u

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ var server = http.createServer(function (req, res) {
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     var tbod = "<tbody>";
-    var tbody = "-";
+    var tbody = "<span></span>";
     var resulth = `<!DOCTYPE html>
         <html lang="en">        
         <head>
@@ -207,7 +207,7 @@ var server = http.createServer(function (req, res) {
                     });
                 }
                 console.log(tbody);
-                resulth += `${tbod}</tbody></table></div></body>
+                resulth += `${tbody}</tbody></table></div></body>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
