6873799867828b6e6e86abf21265cc1812c55558
l1g4v <user_l1g4v@users.noreply.github.com>
2018-05-26 10:01:59 -0500

    u

--- app.js
+++ app.js
@@ -176,11 +176,15 @@ var server = http.createServer(function (req, res) {
     }
     scriptu += `"end"];
     for(var x=0;x<apis.lenght-1;x++){
-        $.getJSON(apis[x], function(data) {
-        console.log(data);
-        document.getElementById(x+"_h").innerHTML=data.pools.ponycoin.hashrateString;
-        document.getElementById(x+"_w").innerHTML=data.pools.ponycoin.workerCount;
-    });
+        $.ajax({
+            url: apis[x],
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+                document.getElementById(x + "_h").innerHTML = data.pools.ponycoin.hashrateString;
+                document.getElementById(x + "_w").innerHTML = data.pools.ponycoin.workerCount;
+            }
+        });
     }
 }
 data();
--- update_code.js
+++ update_code.js
@@ -1,10 +1,14 @@
-function data(){
-    var apis=[];
-    for(var x=0;x<apis.lenght-1;x++){
-        $.getJSON(apis[x], function(data) {
-        console.log(data);
-        document.getElementById(x+"_h").innerHTML=data.pools.ponycoin.hashrateString;
-        document.getElementById(x+"_w").innerHTML=data.pools.ponycoin.workerCount;
-    });
+function data() {
+    var apis = [];
+    for (var x = 0; x < apis.lenght - 1; x++) {
+        $.ajax({
+            url: apis[x],
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+                document.getElementById(x + "_h").innerHTML = data.pools.ponycoin.hashrateString;
+                document.getElementById(x + "_w").innerHTML = data.pools.ponycoin.workerCount;
+            }
+        });
     }
 }
